ae4493269c90bacb615d03bb50190449a9f13633
MuhammadKarimov-dev <karimovdev01@gmail.com>
2025-02-28 13:27:24 +0500

    mages

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import disableDevtool from "disable-devtool";
+
+// disableDevtool({
+//   ondevtoolopen: () => {
+//     console.log("DevTools ochildi!"); // DevTools ochilganda console'ga xabar chiqaradi
+//     alert("Developer Tools’ni ishlatish taqiqlangan!"); // Ogohlantirish chiqaradi
+//   },
+//   ondevtoolclose: () => {
+//     console.log("DevTools yopildi!"); // DevTools yopilganda console'ga xabar chiqaradi
+//   },
+//   url: "https://google.com", // Agar DevTools ochilsa, foydalanuvchini ushbu sahifaga yo‘naltiradi
+//   disableMenu: false, // O'ng tugmachani bosganda "Inspect" menyusini o‘chiradi
+// });
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/pages/OvozAktyori/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Ovoz aktyori Home</div>
+  )
+}
+
+export default Home
